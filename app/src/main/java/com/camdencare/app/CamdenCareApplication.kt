d6bb67767c36d6a77db56710d5ac176537e9e85e
minhaj_irv <minhaj.nadeem@irvinetek.com>
2020-05-14 11:47:06 +0500

    added logging

--- app/src/main/java/com/camdencare/app/CamdenCareApplication.kt
+++ app/src/main/java/com/camdencare/app/CamdenCareApplication.kt
@@ -2,6 +2,7 @@ package com.camdencare.app
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import timber.log.Timber
 
 class CamdenCareApplication : Application() {
 
@@ -10,5 +11,10 @@ class CamdenCareApplication : Application() {
 
         //init stetho
         Stetho.initializeWithDefaults(this)
+
+        //init timber
+        if (BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
