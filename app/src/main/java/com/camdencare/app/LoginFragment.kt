f537607bd2fb98d76659430556441e79d94fda70
Asad Fareed <46973104+asadfareed999@users.noreply.github.com>
2020-05-15 15:10:26 +0500

    login updates

--- app/src/main/java/com/camdencare/app/LoginFragment.kt
+++ app/src/main/java/com/camdencare/app/LoginFragment.kt
@@ -5,20 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.navigation.findNavController
 import com.camdencare.app.R
 import com.camdencare.app.networking.BaseWebservices
 import com.camdencare.app.networking.OnResponseListener
 import com.camdencare.app.networking.responsemodels.ResponseLogin
-import retrofit2.Call
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+
 
 class LoginFragment() : Fragment() {
 
     private lateinit var enterButton: Button
     private val apiEndpointClient = BaseWebservices.getApiEndpointClient()
     private lateinit var loginApiListener: OnResponseListener<ResponseLogin>
+    private lateinit var progressBar: ProgressBar
+    private lateinit var inputLayoutMRN: TextInputLayout
+    private lateinit var inputLayoutNumber: TextInputLayout
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +31,9 @@ class LoginFragment() : Fragment() {
     ): View {
         val view: View = inflater.inflate(R.layout.fragment_login, container, false)
         enterButton = view.findViewById(R.id.btn_enter)
+        progressBar= requireActivity().findViewById(R.id.loadingBar)
+        inputLayoutMRN=view.findViewById(R.id.textfield_mrn)
+        inputLayoutNumber=view.findViewById(R.id.textfield_mobile_number)
         enterButton.setOnClickListener {
             val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment()
             // view.findNavController().navigate(action) }
@@ -35,8 +43,20 @@ class LoginFragment() : Fragment() {
     }
 
     private fun exeLoginApi() {
-        val call = apiEndpointClient.login("80394651", "03321745601")
-        BaseWebservices.executeApi(call, loginApiListener)
+        val mrn:String=inputLayoutMRN.editText!!.text.toString()
+        val number:String=inputLayoutNumber.editText!!.text.toString()
+        if (mrn.isEmpty()){
+            inputLayoutMRN.isErrorEnabled=true
+            inputLayoutMRN.error="Enter a valid MRN"
+        }else if (number.isEmpty()){
+            inputLayoutNumber.isErrorEnabled=true
+            inputLayoutNumber.error="Enter a valid Number"
+        } else {
+            progressBar.visibility=View.VISIBLE
+            progressBar.isIndeterminate=true
+            val call = apiEndpointClient.login(mrn, number)
+            BaseWebservices.executeApi(call, loginApiListener)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,13 +64,21 @@ class LoginFragment() : Fragment() {
         
         loginApiListener = object : OnResponseListener<ResponseLogin> {
             override fun onSuccess(response: ResponseLogin?) {
-
+                progressBar.visibility=View.GONE
+                val MRN:String=response!!.mrn
+                val FullName:String=response.fullName
+                val Age:String=response.age
+                Snackbar.make(view, "Login Success", Snackbar.LENGTH_LONG).show()
             }
 
             override fun onFailure(t: Throwable) {
+                progressBar.visibility=View.GONE
+                Snackbar.make(view, t.message.toString(), Snackbar.LENGTH_LONG).show()
             }
 
             override fun onCancel() {
+                progressBar.visibility=View.GONE
+                Snackbar.make(view, "Login Cancelled", Snackbar.LENGTH_LONG).show()
             }
 
         }
