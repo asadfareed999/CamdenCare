1183144d137b549389faf563b493c6debf563873
minhaj_irv <minhaj.nadeem@irvinetek.com>
2020-05-19 13:17:57 +0500

    Update CamdenCarePreferences.kt

--- app/src/main/java/com/camdencare/app/prefrences/CamdenCarePreferences.kt
+++ app/src/main/java/com/camdencare/app/prefrences/CamdenCarePreferences.kt
@@ -7,29 +7,44 @@ import com.camdencare.app.utilities.STR_PREF_NAME
 class CamdenCarePreferences(private val context: Context) :
     BasePreference(context, STR_PREF_NAME) {
 
-    fun isLogin():Boolean{
-        val mrn=getMrn()
+    companion object {
+        const val DEF_VALUE_STRING = ""
+
+        const val PREF_KEY_MRN = "mrn_key"
+        const val PREF_KEY_NAME = "name_key"
+        //todo:define other keys
+    }
+
+    fun isLogin(): Boolean {
+        val mrn = getMrn()
         return !mrn.isEmpty()
     }
-    fun saveMRN(mrn:String){
-        putString("mrn_key",mrn)
+
+    fun saveMRN(mrn: String) {
+        putString(PREF_KEY_MRN, mrn)
     }
-    fun saveName(name:String){
-        putString("name_key",name)
+
+    fun saveName(name: String) {
+        putString(PREF_KEY_NAME, name)
     }
-    fun saveAge(age:String){
-        putString("age_key",age)
+
+    fun saveAge(age: String) {
+        putString("age_key", age)
     }
-    fun getMrn():String{
-        return getString("mrn_key","")!!
+
+    fun getMrn(): String {
+        return getString("mrn_key", "")!!
     }
-    fun getName():String{
-        return getString("name_key","")!!
+
+    fun getName(): String {
+        return getString("name_key", "")!!
     }
-    fun getAge():String{
-        return getString("age_key","")!!
+
+    fun getAge(): String {
+        return getString("age_key", "")!!
     }
-    fun logout(){
+
+    fun logout() {
         clearSharedPrefs()
     }
 }
